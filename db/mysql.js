40dc5a9cd5d6da3359936318aad81973dd10658d
Harit tilavat <harittilavat10@gmail.com>
2024-03-22 20:11:37 +0530

    added new files

--- db/mysql.js
+++ db/mysql.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const dbConfig = {
-    connectionLimit: 10,
+    connectionLimit: 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
